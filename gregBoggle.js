9f8604345a0acdd7d33a201ffec0ae936bcdada5
davidvilla62 <77689256+davidvilla62@users.noreply.github.com>
2021-03-28 20:45:25 -0500

    Add files via upload

---
+++ gregBoggle.js
@@ -0,0 +1,22 @@
+function words(x)
+{   var word;
+	switch (x)
+	{
+	case 1:
+		word = new Array("balte","table","hat","tab","belt","lab","eat","tea","ate","tale","bale","let","bet","teal","late","beat");
+		break;		
+	case 2:
+		word = new Array("atwre","water","wet","wear","tear","war","ret","rate","eat","ate","tea","awe","raw","rat","wart","art","tar");
+		break;
+	case 3:
+		word = new Array("dclaen","can","cane","and","clan","lane","lean","lend","land","den","dean","dance","lance","clean","deal","ale","dale","candle","clad");
+		break;
+	case 4:
+		word = new Array("aepinlar","air","airplane","plane","plan","lane","lean","pane","ear","near","nap","nape","lair","pen","pan","ape","leap","ale","peal","nap","rap","par", "pare", "pale", "are", "rail", "ail", "pail", "nail", "air", "pair", "ran", "pin", "pine", "line", "nip", "rip", "ripe", "lip", "earn", "learn", "ire");
+		break;
+	case 5:
+		word = new Array("redykboa","keyboard","key","board","bored","bore","bark","dark","dork","oar","boar","ark","dare","bare","are","red","rod","road","bode","rode","ode","bread", "read", "bead", "bred", "break", "drey", "day", "boy", "broke", "rake", "bake", "ear", "dear", "bear", "dye", "dyer", "doer", "oak", "boa", "doe", "okay","dab", "bade", "ade", "drake", "bard", "yard", "year", "beak", "beard", "bad", "bed", "bay");
+		break;
+	}
+	return word;
+}
